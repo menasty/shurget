2a326579a46397c92ef7f94d99549f0fddf33fef
menasty <mweinhold86@gmail.com>
2026-06-22 05:24:34 +0000

    fix: properly configure EJS view engine in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configure EJS templates
+// Set up EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
